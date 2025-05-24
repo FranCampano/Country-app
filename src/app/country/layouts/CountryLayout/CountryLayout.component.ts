a1de5fec91d2ca2d173f5da03b9e18390ad12b1c
FranCampanoDiaz <campanodiazfrancisco@gmail.com>
2025-05-24 22:41:57 +0200

    creacion botones y tabla graficamente

--- src/app/country/layouts/CountryLayout/CountryLayout.component.ts
+++ src/app/country/layouts/CountryLayout/CountryLayout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "../../../shared/components/footer/footer.component";
+import { TopMenuComponent } from "../../components/top-menu/top-menu.component";
 
 @Component({
   selector: 'app-country-layout',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, TopMenuComponent],
   templateUrl: './CountryLayout.component.html',
 })
 export class CountryLayoutComponent { }
